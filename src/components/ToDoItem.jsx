a6377e2f3846fa2e8e74c274b4bf42250fd91908
AlexDegerman <alex.degerman.dev@gmail.com>
2025-03-05 00:22:44 +0200

    Add creation date to tasks

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -2,6 +2,15 @@ import { Trash2Icon } from "lucide-react"
 
 const ToDoItem = ({ task, toggleComplete, deleteTask }) => {
 
+  const formatDate = (timestamp) => {
+    const options = {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric"
+    }
+    return new Date(timestamp).toLocaleDateString("en-GB", options)
+  }
+
   return (
     <div>
       <input 
@@ -10,6 +19,7 @@ const ToDoItem = ({ task, toggleComplete, deleteTask }) => {
         onChange={() => toggleComplete(task.id)}
       />
       <span> {task.text} </span>
+      <span> {formatDate(task.id)} </span>
       <button onClick={() => deleteTask(task.id)}><Trash2Icon/></button>
     </div>
   )
